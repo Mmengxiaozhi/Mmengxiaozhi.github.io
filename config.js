97683d77364bfb8b33b025629e722f9961d2d201
Mmengxiaozhi <74833412+Mmengxiaozhi@users.noreply.github.com>
2020-12-01 00:05:42 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,56 @@
+var CONFIG = {
+  "maps": {
+    "o1": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1606749839,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 11,
+      "name": "Miaoli",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        2
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "overworld_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "overworld",
+      "worldName": "overworld",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "o1"
+  ],
+  "tileSetGroups": {
+    "overworld_isometric_t1": {
+      "maxZoom": 11,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
